fa6fe6e1d6bfc56bbcdef2a7b13d08c2a928f406
antonyloose <antony35007@gmail.com>
2025-03-06 20:04:40 +1100

    feat: update game loop

--- build/game/game-loop.js
+++ build/game/game-loop.js
@@ -1,5 +1,12 @@
 export class Game_Loop {
-    constructor() { }
+    constructor(scene) {
+        this._scene = scene;
+        const canvas = scene.get_canvas();
+        document.body.appendChild(canvas);
+    }
     tick(elapsed_millis) {
+        const canvas = this._scene.get_canvas();
+        this._scene.clear_canvas();
+        this._scene.get_grid().draw(canvas);
     }
 }
--- src/game/game-loop.ts
+++ src/game/game-loop.ts
@@ -1,6 +1,19 @@
+import { Scene } from "./scene.js";
+
 export class Game_Loop {
-    constructor() { }
+    private _scene: Scene;
+
+    public constructor(
+        scene: Scene
+    ) {
+        this._scene = scene;
+        const canvas = scene.get_canvas();
+        document.body.appendChild(canvas);
+    }
 
     public tick(elapsed_millis: number) {
+        const canvas = this._scene.get_canvas();
+        this._scene.clear_canvas();
+        this._scene.get_grid().draw(canvas);
     }
 }
