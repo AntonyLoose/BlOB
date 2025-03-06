08cd2a18bd61ac830c7e6dc27b21c84c87c34f6d
antonyloose <antony35007@gmail.com>
2025-03-06 17:33:59 +1100

    feat: create camera

---
+++ build/game/camera.js
@@ -0,0 +1,5 @@
+export class Camera {
+    constructor(y = 0) {
+        this.y = y;
+    }
+}
---
+++ src/game/camera.ts
@@ -0,0 +1,9 @@
+export class Camera {
+    public y: number;
+
+    public constructor(y: number = 0) {
+        this.y = y;
+    }
+
+    // TODO: follow player method
+}
